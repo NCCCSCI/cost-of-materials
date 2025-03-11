77584bd72e9e245758785bf42257fb08f66705e2
Strawb <psmith128@students.ccsnh.edu>
2025-03-11 16:17:58 -0400

    Two Months

--- get_updates.php
+++ get_updates.php
@@ -1,6 +1,52 @@
 <?php
 #!/usr/bin/php
 
+$phpInput = trim(file_get_contents('php://input'));
+$jsonData = json_decode($phpInput,true);
+
+$args              = [
+    'store_id' => ['filter' => FILTER_VALIDATE_REGEXP, 'options' => ['regexp' => '/^\w{1,64}$/']],
+    'username' => ['filter' => FILTER_VALIDATE_REGEXP, 'options' => ['regexp' => '/^\w{1,64}$/']],
+    'password' => ['filter' => FILTER_VALIDATE_REGEXP, 'options' => ['regexp' => '/^\w{1,64}$/']]
+];
+
+$data   = filter_var_array($jsonData, $args);
+if (!is_array($data)) {
+    halt(401);
+}
+
+$storeId = $data['store_id'];
+$username = $data['username'];
+$password = $data['password'];
+if (empty($storeId) || empty($username) || empty($password)) {
+    halt(401);
+}
+
+$ftp_server = 'REDACTED';
+$ftp_port = 0xEDAC;
+
+// set up a connection or die
+$connection = ssh2_connect($ftp_server,$ftp_port) or die("Couldn't connect");
+if (!ssh2_auth_password($connection, $username, $password)) {
+    halt(403,'Invalid credentials');
+}
+$remote = '/FMS'.$storeId.'_HEOAExtract.zip';
+$tmp = tempnam(sys_get_temp_dir(),'HEOA_');
+
+$sftp = ssh2_sftp($connection);
+$stream = fopen('ssh2.sftp://' . intval($sftp) .'/.'. $remote, 'r');
+if ($stream === false) {
+    halt(502,'File open failed');
+}
+$bytesWritten = file_put_contents($tmp,$stream);
+fclose($stream);
+ssh2_disconnect($connection);
+
+
+if ($bytesWritten === false){
+    halt(500,'File write failed');
+}
+
 $temp = parse_ini_file(".env");
 
 $ftp_server = $temp["server"];
