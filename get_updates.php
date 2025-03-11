3e62cacfd0aded237f798a48d52f61ee7bc4a81d
Strawb <psmith128@students.ccsnh.edu>
2025-03-11 16:29:56 -0400

    Another small update

--- get_updates.php
+++ get_updates.php
@@ -1,4 +1,5 @@
 <?php
+
 #!/usr/bin/php
 
 $temp = parse_ini_file(".env");
@@ -10,32 +11,31 @@ $username = $temp["username"];
 $password = $temp["password"];
 
 // set up a connection or die
-$connection = ssh2_connect($ftp_server,$ftp_port) or die("Couldn't connect");
+$connection = ssh2_connect($ftp_server, $ftp_port) or die("Couldn't connect");
 if (!ssh2_auth_password($connection, $username, $password)) {
-    halt(403,'Invalid credentials');
+    halt(403, 'Invalid credentials');
 }
-$remote = '/FMS'.$storeId.'_HEOAExtract.zip';
-$tmp = tempnam(sys_get_temp_dir(),'HEOA_');
+$remote = '/FMS' . $storeId . '_HEOAExtract.zip';
+$tmp = tempnam(sys_get_temp_dir(), 'HEOA_');
 
 $sftp = ssh2_sftp($connection);
-$stream = fopen('ssh2.sftp://' . intval($sftp) .'/.'. $remote, 'r');
+$stream = fopen('ssh2.sftp://' . intval($sftp) . '/.' . $remote, 'r');
 if ($stream === false) {
-    halt(502,'File open failed');
+    halt(502, 'File open failed');
 }
-$bytesWritten = file_put_contents($tmp,$stream);
+$bytesWritten = file_put_contents($tmp, $stream);
 fclose($stream);
 ssh2_disconnect($connection);
 
-
-if ($bytesWritten === false){
-    halt(500,'File write failed');
+if ($bytesWritten === false) {
+    halt(500, 'File write failed');
 }
 
 $zip = new ZipArchive;
 $zipOkay = true;
 if ($zip->open($tmp) === TRUE) {
-    $unzipped = $tmp.'-unzip/';
-    mkdir($unzipped,0700);
+    $unzipped = $tmp . '-unzip/';
+    mkdir($unzipped, 0700);
     $zip->extractTo($unzipped);
     $zip->close();
 } else {
@@ -48,30 +48,17 @@ if (!$zipOkay) {
     halt(500, 'Unzip failed');
 }
 
-$txt = $unzipped.str_replace('zip','txt',$tmp);
+$txt = $unzipped . str_replace('zip', 'txt', $tmp);
 
- if (exec('clamscan '.escapeshellarg($txt), $output, $resultCode) === false) {
-     halt(500, 'Virus scan failed');
- }
-
- if($resultCode !== 0) {
-     halt(500, 'Virus');
- }
-
-// Thanks to: https://www.php.net/manual/en/function.readfile.php
-if (file_exists($txt)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.basename($txt).'"');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($txt));
-    readfile($txt);
-    unlink($txt);
-    rmdir($unzipped);
-    exit;
+if (exec('clamscan ' . escapeshellarg($txt), $output, $resultCode) === false) {
+    halt(500, 'Virus scan failed');
 }
+
+if ($resultCode !== 0) {
+    halt(500, 'Virus');
+}
+
+exit(0);
 
 function halt($code, $message = '') {
     echo $message;
